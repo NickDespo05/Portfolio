b4b18952900db80a774fdcd0d354ac12f0147af2
Nick D'Esposito <Nicholas D'Esposito>
2022-06-23 13:20:16 -0400

    Made transitions with completed formatting for projects

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function revealElement(className) {
+    window.addEventListener("scroll", () => {
+        var reveals = document.querySelectorAll(className);
 
+        for (var i = 0; i < reveals.length; i++) {
+            var windowHeight = window.innerHeight;
+            var revealTop = reveals[i].getBoundingClientRect().top;
+            var revealPoint = 100;
+
+            if (revealTop < windowHeight - revealPoint) {
+                reveals[i].classList.add("active");
+            } else {
+                reveals[i].classList.remove("active");
+            }
+        }
+    });
+}
+
+revealElement(".reveal");
